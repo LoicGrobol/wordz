12fa8df49811de0b4b84ba1045b3f77fdb16d018
L. Grobol <lgrobol@parisnanterre.fr>
2025-01-21 14:21:07 +0100

    add support for specific word selection

--- wordz.py
+++ wordz.py
@@ -12,13 +12,23 @@ def compte_mots(texte: Iterable[str]) -> Counter[str]:
 
 
 def main():
-    inpt = sys.argv[1]
+    try:
+        inpt = sys.argv[1]
+    except IndexError:
+        print("Invalid command. Usage: wordz INPT [WORD].")
+        return
 
     with open(inpt) as in_stream:
         count = compte_mots(in_stream)
 
-    for w, c in count.most_common(16):
-        print(f"{w}: {c}")
+    if len(sys.argv) == 3:
+        w = sys.argv[2]
+        print(f"{w}: {count[w]}")
+    elif len(sys.argv) == 2:
+        for w, c in count.most_common(16):
+            print(f"{w}: {c}")
+    else:
+        print("Invalid command. Usage: wordz INPT [WORD].")
 
 if __name__ == "__main__":
     main()
